5998c5f68653b736e27edf9fd33738a3ecdb794b
Francesco Porcari <francesco.porcari@softwell.it>
2014-05-31 19:00:42 +0200

    a

--- packages/sandbox/webpages/Lessons/Chapter 1/01_hello_world.py
+++ packages/sandbox/webpages/Lessons/Chapter 1/01_hello_world.py
@@ -1,7 +1,5 @@
 # -*- coding: UTF-8 -*-
             
 class GnrCustomWebPage(object):
-    dojo_source=True
     def main(self,root,**kwargs):
         root.div('Hello world')
-        root.div('ciao')
