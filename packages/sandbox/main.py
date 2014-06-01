7fd56dae05dc6fd341e762b94a2c82120367fd60
gporcari <giovanni.porcari@gmail.com>
2014-06-01 09:35:57 +0200

    Changes to have sandbox as main package

--- packages/sandbox/main.py
+++ packages/sandbox/main.py
@@ -5,7 +5,7 @@ from gnr.app.gnrdbo import GnrDboTable, GnrDboPackage
 class Package(GnrDboPackage):
     def config_attributes(self):
         return dict(comment='sandbox package',sqlschema='sandbox',
-                    name_short='Sandbox', name_long='Sandbox', name_full='Sandbox')
+                    name_short='Sandbox', name_long='Genropy Sandbox', name_full='Sandbox')
                     
     def config_db(self, pkg):
         pass
