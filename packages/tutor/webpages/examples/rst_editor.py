d49d646fae2700820cf3e960cafff1d4f8df5b58
gporcari <giovanni.porcari@gmail.com>
2014-06-02 16:51:50 +0200

    ssss

--- packages/tutor/webpages/examples/rst_editor.py
+++ packages/tutor/webpages/examples/rst_editor.py
@@ -1,40 +1,26 @@
 # -*- coding: UTF-8 -*-
 
 from gnr.core.gnrdecorator import public_method
-from gnr.core.gnrbag import Bag
+from docutils.core import publish_string
 
 class GnrCustomWebPage(object):
+    
     def main(self,root,**kwargs):
         bc = root.borderContainer(datapath='main')
-        left = bc.framePane(region='left',width='50%',splitter=True,margin='2px',margin_right=0,border='1px solid #efefef')
-        left.center.contentPane(overflow='hidden').codemirror(value='^.source',height='100%',
-               config_mode='rst',config_lineNumbers=True,config_keyMap='softTab')
-        
-        editorbar = left.top.slotToolbar('5,editmenu,*',height='22px')
-        editorbar.data('.editor.menuoptions',self.editor_menuoptions())
-        editorbar.editmenu.dropDownButton('Edit').menu(storepath='.editor.menuoptions',
-                                                      action='alert($1.code);',_class='smallMenu')
-
-        right = bc.framePane(region='center',margin='2px',border='1px solid #efefef')
-        renderingbar = right.top.slotToolbar('5,style_selector,*',height='22px')
-        fb = renderingbar.style_selector.formbuilder(cols=1,border_spacing='0px')
-        fb.filteringSelect(value='^.rendering.theme',lbl='Theme',
-                            values='violet:Violet,gray:Gray,standard:Standard')
-        iframe = right.center.contentPane(overflow='hidden').htmliframe(height='100%',width='100%',border=0)
-        bc.dataController('iframe.domNode.contentWindow.document.body.innerHTML = rendering',
-                            rendering='^.rendering.html',
-                            iframe=iframe)
-        bc.dataRpc('.rendering.html',self.convert_rst2html,source_rst='^.source',
-                    theme='^.rendering.theme',
+        left = bc.contentPane(region='left',width='50%',splitter=True,overflow='hidden')
+        left.codemirror(value='^.source',height='100%',
+               border='1px solid silver',config_mode='rst',config_lineNumbers=True) 
+        iframe = bc.contentPane(region='center').htmliframe(height='100%',width='100%',border=0)
+        bc.dataController('iframe.domNode.contentWindow.document.body.innerHTML = rendering',rendering='^.rendering',iframe=iframe)
+        bc.dataRpc('.rendering',self.convert_rst2html,_onCalling="""kwargs.source_rst=' \n'+skwargs.source_rst;""",
+                    source_rst='^.source',
                     _delay=500)
 
-    def editor_menuoptions(self):
-        result = Bag()
-        result.setItem('bold',None,caption='Bold',code='bold')
-        result.setItem('italic',None,caption='Italic',code='italic')
-        return result
+
 
     @public_method
-    def convert_rst2html(self,source_rst=None,theme=None,**kwargs):
-        return self.site.getService('rst2html')(source_rst,
-                                    theme=theme,**kwargs)
+    def convert_rst2html(self,source_rst=None,**kwargs):
+        stylesheet_path=self.site.getStaticPath('rsrc:common','rstcss','gnrcss.css')
+        settings_overrides = {'embed_stylesheet': 'yes','stylesheet_path':stylesheet_path}
+        return publish_string(source_rst, writer_name='html',
+              settings_overrides=settings_overrides)
