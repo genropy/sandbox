dd3bd854d421f3fd8cca4560349e467b0a231e9c
Francesco Porcari <francesco.porcari@softwell.it>
2014-06-02 15:50:57 +0200

    a

--- packages/sandbox/webpages/index.py
+++ packages/sandbox/webpages/index.py
@@ -2,10 +2,9 @@
             
 class GnrCustomWebPage(object):
     py_requires = 'frameindex'
-
-
     def loginSubititlePane(self,pane):#here you can define the sub title as you required a 
         pane.div('Please log in User:sandbox Password:sandbox',
                     text_align='center',
                     font_size='.9em',
                     font_style='italic',color='#AAAAAA')
+
