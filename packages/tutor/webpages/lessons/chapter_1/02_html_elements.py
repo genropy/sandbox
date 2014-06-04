c0d9f01e77373dfb0ccd8101cbab547c7ce6840d
gporcari <giovanni.porcari@gmail.com>
2014-06-04 09:00:55 +0200

    work in progress

--- packages/tutor/webpages/lessons/chapter_1/02_html_elements.py
+++ packages/tutor/webpages/lessons/chapter_1/02_html_elements.py
@@ -1,29 +1,35 @@
 # -*- coding: UTF-8 -*-
+
+from random import randint as rn
             
 class GnrCustomWebPage(object):    
     def main(self,root,**kwargs):
-        root.div('Some Genropy stuff',margin='10px',font_size='24px',
-                                      color='#444') 
+        root.div('We use now some HTML elements',margin='10px',
+                      font_size='24px',text_align='center',
+                      color='#444') 
         #we receive a root and we add a div with a content and
         #some attributes.
         
-        container=root.div(height='150px',width='300px',margin='10px',
-                         border='1px solid gray',rounded='4') 
-        #we add now to the root a div that has not a content, just attributes.
-        #now this div is in a python variable named 'container'
+        mybox=root.div(height='150px',width='400px',margin='auto',
+                         padding='10px',
+                         border='1px solid gray',rounded='10',
+                         margin_top='50px',shadow='4px 4px 8px #666') 
+        #we add now to the root a div that has not a content, 
+        #just attributes.
+        #this div is in a python variable named 'mybox'
         #we can now put some elements in container
                          
-        for j in range(98):
-            container.div(height='15px',width='15px',background='red',
-                       shadow='2px 2px 3px #666',margin='3px',
-                       rounded='4',float='left')
+        for j in range(96):
+            mybox.div(height='15px',width='15px',shadow='2px 2px 3px #666',
+                      background_color='rgb(%i,%i,%i)'%(rn(0,255),rn(0,255),rn(0,255)),
+                      rounded='10',float='left',margin='5px')
         
-        mytable=root.table(margin='40px',background='#666',
+        mytable=root.table(margin='auto',margin_top='50px',
                            border_spacing=0,border_collapse='collapse')
-        tbody=mytable.tbody(font_size='10px',color='white')
+        tbody=mytable.tbody(font_size='10px',color='gray')
         for r in range (10):
             row=tbody.tr()
             for c in range (10):
                 row.td(padding='2px').div('cell<br/>%i-%i'%(r,c),
-                                          padding_right='8px',padding_left='8px',
-                         rounded=4,border='1px solid white')
+                                          padding_right='6px',padding_left='6px',
+                         rounded=4,border='1px solid gray')
