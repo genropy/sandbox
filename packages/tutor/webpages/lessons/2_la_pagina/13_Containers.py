feca4b990306b2cc357acda070a3ceb4386a1366
gporcari <giovanni.porcari@gmail.com>
2014-06-19 13:41:04 +0200

    fff

--- packages/tutor/webpages/lessons/2_la_pagina/13_Containers.py
+++ packages/tutor/webpages/lessons/2_la_pagina/13_Containers.py
@@ -2,7 +2,7 @@
 import datetime
 from dateutil.rrule import rrule,DAILY
 from dateutil.relativedelta import relativedelta as rd
-from gnr.core.gnrdecorator import public_method
+from gnr.core.gnrdecorator import public_method,extract_kwargs
 from gnr.core.gnrbag import Bag
 
 class GnrCustomWebPage(object):
@@ -10,49 +10,50 @@ class GnrCustomWebPage(object):
     def main(self,root,**kwargs):
         bc =root.borderContainer(height='100%',datapath='italia',
                                  margin='15px', border='1px solid silver')
-        left=bc.contentPane(region='left',width='200px',datapath='.regioni',
-                            splitter=True)
-        left.h1('Regioni')
-        left.quickGrid(value='^.data',height='100%',width='auto',
-                       border_right='1px solid silver',
-                       selected_sigla='.selected',autoSelect=True)
-        left.dataRpc('.data',self.fillGrid,table='glbl.regione',
-                     columns='$sigla,$nome',_onStart=True)
-        center_bc=bc.borderContainer(region='center')
-
-        left=center_bc.contentPane(region='left',width='150px',datapath='.province',
-                                   splitter=True)
-        left.h1('Province')
-        left.quickGrid(value='^.data',height='100%',width='auto',
-                       border_right='1px solid silver',
-                       selected_nome='.selected',autoSelect=True)
-        left.dataRpc('.data',self.fillGrid,table='glbl.provincia',
-                     columns='$sigla,$nome',where="$regione = :regione",
-                     regione='^italia.regioni.selected')
-        
-        
-    def comuni(self,tc):
-        bc =tc.borderContainer(datapath='comuni')
-        top=bc.contentPane(region='top',height='26px',border_bottom='1px solid silver')
-        fb=top.formbuilder(cols=1)
-        fb.dbSelect(value='^.pr',dbtable='glbl.provincia',lbl='Provincia')
-        left=bc.contentPane(region='left',width='150px',
-                            border_right='1px solid grey',splitter=True)
-        left.quickGrid(value='^.data',
-                        selected_denominazione='.selected_comune',width='auto')
-        left.dataRpc('.data',self.getComuni, provincia='^.pr')
-        
-        center=bc.borderContainer(region='center')
-        pane=center.contentPane(region='center')
+                                 
+        center=self.tableIndex(bc,title='Regioni',datapath='.regioni',
+                           table='glbl.regione',columns='$sigla,$nome',
+                           _onStart=True, grid_selected_sigla='.selected')
+         
+        center=self.tableIndex(center,title='Province',datapath='.province',
+                           table='glbl.provincia',columns='$sigla,$nome',
+                           where="$regione = :regione",
+                           regione='^italia.regioni.selected',
+                           grid_selected_sigla='.selected')
+                           
+        pane=self.tableIndex(center,title='Comuni',datapath='.comuni',
+                           table='glbl.comune',columns='$denominazione',
+                           where="$sigla_provincia = :provincia",
+                           provincia='^italia.province.selected',
+                           grid_selected_denominazione='.selected')
+                           
         pane.htmliframe(width='100%',height='100%',
-                        src='^.wikipedia',border=0,
-                        visible='^.selected_comune')
+                        src='^.wikipedia',border=0)
         pane.dataFormula('.wikipedia',"'http://it.wikipedia.org/wiki/'+comune",
-                     comune='^.selected_comune',_if='comune',_else="'/'")
+                     comune='^italia.comuni.selected',_if='comune',_else="'/'")
+                     
+    @extract_kwargs(grid=True)
+    def tableIndex(self,bc,title=None,datapath=None,table=None,columns=None,
+                        where=None,grid_kwargs=None,**kwargs):
+                        
+        pane=bc.borderContainer(region='left',width='180px',datapath=datapath,
+                            splitter=True)
+                            
+        pane.contentPane(region='top').div(title,text_align='center',
+                                           color='white',background_color='#444')
+                                           
+        pane.contentPane(region='center').quickGrid(value='^.data',
+                                             height='100%',width='100%',
+                                             border_right='1px solid silver',
+                                             autoSelect=True,**grid_kwargs)
+                                             
+        pane.dataRpc('.data',self.fillGrid,table=table,where=where,
+                       columns=columns,**kwargs)
+                       
+        return bc.borderContainer(region='center') 
         
     @public_method
     def fillGrid(self, table=None, columns=None, where=None, **kwargs):
-        print 'table ',table, where,kwargs
         table=self.db.table(table)
         result=Bag()
         selection=table.query(columns=columns,addPkeyColumn=False,
