8213b24376ca5d76342bd104faf968c9d2d1f205
gporcari <giovanni.porcari@gmail.com>
2014-06-19 08:50:20 +0200

    c

--- packages/tutor/webpages/lessons/2_la_pagina/13_Containers.py
+++ packages/tutor/webpages/lessons/2_la_pagina/13_Containers.py
@@ -2,94 +2,62 @@
 import datetime
 from dateutil.rrule import rrule,DAILY
 from dateutil.relativedelta import relativedelta as rd
+from gnr.core.gnrdecorator import public_method
+from gnr.core.gnrbag import Bag
+
 class GnrCustomWebPage(object):
     
     def main(self,root,**kwargs):
-        self.testTc(root.div(datapath='testTc',margin='10px'))
-        self.testBc(root.div(datapath='testBc',margin='10px'))
-        
-        
-    def testTc(self,pane):
-        tc=pane.tabContainer(height='300px',width='550px')
-        c=tc.contentPane(title='Clock',datapath='.clock',padding='5px')
-        c.div('^.showtime',font_size='130px',text_align='center',
-              margin_top='50px',color='#2A7ACC')
-        c.dataFormula('.showtime',"_F(new Date(),'hh:mm:ss','H')",
-                               _timing=1)
-        notes = tc.contentPane('^.text',title='Notes',datapath='.notes',
-                               padding='5px')
-        notes.simpleTextArea(height='100%',width='100%',editor=True)
-        innertc=tc.tabContainer(title='Calendar',margin='4px',
-                                datapath='.calendar',height='100%')
-        for week in range(4):
-            self.makeWeek(innertc,week)
+        bc =root.borderContainer(height='100%',datapath='italia',
+                                 margin='15px', border='1px solid silver')
+        left=bc.contentPane(region='left',width='200px',datapath='.regioni',
+                            splitter=True)
+        left.h1('Regioni')
+        left.quickGrid(value='^.data',height='100%',width='auto',
+                       border_right='1px solid silver',
+                       selected_sigla='.selected',autoSelect=True)
+        left.dataRpc('.data',self.fillGrid,table='glbl.regione',
+                     columns='$sigla,$nome',_onStart=True)
+        center_bc=bc.borderContainer(region='center')
 
-    def makeWeek(self,pane,week=None):
-        tw=pane.contentPane(title='Week %i' % week,
-                                   datapath='.week_%i' % week)
-        statuslist='F:Free,H:Home,W:Work,T:Travel'
-        dtstart=datetime.date.today()
-        dtstart=dtstart+rd(dtstart,weeks=week)
-        tb=tw.table(margin='5px').tbody()
-        h=tb.tr(datapath='week_%i')
-        h.td()
-        days=[d.strftime('%d/%m/%y') for d in rrule(DAILY, count=7, dtstart=dtstart)]
-        for d in days:
-            h.td(d,align='center')
-        for h in range (8,18):
-            r=tb.tr()
-            r.td('%i:00'%h,align='right')
-            for d in days:
-                r.td().filteringSelect('^.%s.%i' %(d,h),width='5em',values=statuslist)
-    
-    
-    def testBc(self,pane):
-
-        bc=pane.borderContainer(height='300px',width='550px',
-                                border='1px solid silver',rounded=8)
-                                
-        top=bc.contentPane(region='top',height='20px',background_color='silver')
-        pages='small:Small,medium:Medium,large:Large'
-        top.filteringSelect('^.page',values=pages,width='10em')
-        
-        fb=bc.contentPane(region='left',width='150px',
-                            border_right='1px solid silver',
-                            splitter=True).formbuilder(cols=1)
-        fb.checkBox('^.italic',label='Italic')
-        fb.checkBox('^.bold',label='Bold')
-        fb.checkBox('^.underline',label='Underline')
-        fb.checkBox('^.small_caps',label='Small Caps')
-        fb.dataController(""" var style='font-weight:'+(bold?'bold':'normal');
-                                  style+=';font-style:'+(italic?'italic':'normal');
-                                  style+=';text-decoration:'+(underline?'underline':'none');
-                                  style+=';font-variant:'+(small_caps?'small-caps':'normal')+';';
-                                  SET .style = style;
-                          """,italic='^.italic',bold='^.bold',
-                             underline='^.underline', small_caps='^.small_caps')
-        center=bc.stackContainer(region='center',margin='3px',selectedPage='^.page')
-        for k,p in enumerate(pages.split(',')):
-            pageName,title=p.split(':')
-            c=center.contentPane(pageName=pageName,style='^.style')
-            c.div(title,padding='20px',font_size='%ipx' % (20*(k+1)))
-
-        bottom=bc.borderContainer(region='bottom',height='22px',
-                                  background_color='silver')
-        b_left=bottom.contentPane(region='left',width='60px').button('Clear',action='alert("Clearing...")')
-        b_right=bottom.contentPane(region='right',width='60px').button('Submit',action='alert("Submitting...")')
-        c_center=bottom.contentPane(region='center')
-        c_center.div('Genropy',text_align='center',font_size='20px')
-       
+        left=center_bc.contentPane(region='left',width='150px',datapath='.province',
+                                   splitter=True)
+        left.h1('Province')
+        left.quickGrid(value='^.data',height='100%',width='auto',
+                       border_right='1px solid silver',
+                       selected_nome='.selected',autoSelect=True)
+        left.dataRpc('.data',self.fillGrid,table='glbl.provincia',
+                     columns='$sigla,$nome',where="$regione = :regione",
+                     regione='^italia.regioni.selected')
         
         
-                    
-                  
-                    
+    def comuni(self,tc):
+        bc =tc.borderContainer(datapath='comuni')
+        top=bc.contentPane(region='top',height='26px',border_bottom='1px solid silver')
+        fb=top.formbuilder(cols=1)
+        fb.dbSelect(value='^.pr',dbtable='glbl.provincia',lbl='Provincia')
+        left=bc.contentPane(region='left',width='150px',
+                            border_right='1px solid grey',splitter=True)
+        left.quickGrid(value='^.data',
+                        selected_denominazione='.selected_comune',width='auto')
+        left.dataRpc('.data',self.getComuni, provincia='^.pr')
         
-       
+        center=bc.borderContainer(region='center')
+        pane=center.contentPane(region='center')
+        pane.htmliframe(width='100%',height='100%',
+                        src='^.wikipedia',border=0,
+                        visible='^.selected_comune')
+        pane.dataFormula('.wikipedia',"'http://it.wikipedia.org/wiki/'+comune",
+                     comune='^.selected_comune',_if='comune',_else="'/'")
         
+    @public_method
+    def fillGrid(self, table=None, columns=None, where=None, **kwargs):
+        print 'table ',table, where,kwargs
+        table=self.db.table(table)
+        result=Bag()
+        selection=table.query(columns=columns,addPkeyColumn=False,
+                           where=where,**kwargs).fetch()
+        for k,record in enumerate(selection):
+            result.setItem('r_%i' % k,Bag(record))
+        return result
         
-        
-
-
-        
-   
