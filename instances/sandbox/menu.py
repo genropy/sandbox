29b86ea93eda12197270c9782a1905b1e6d928a2
Francesco Porcari <francesco.porcari@softwell.it>
2015-04-14 16:24:05 +0200

    no message

--- instances/sandbox/menu.py
+++ instances/sandbox/menu.py
@@ -5,16 +5,14 @@ def config(root,application=None):
     tutor.branch("Lezioni", pkg="tutor", dir="lessons")
     tutor.branch("Esempi", pkg="tutor", dir="examples")
     tutor.branch("Esercizi", pkg="tutor", dir="exercises")
-
+    root.branch("Widgetpedia", dir="widgetpedia",pkg='dev')
     fatturazione = root.branch("Fatturazione")
     fatturazione.thpage("Clienti", table="fatt.cliente")
     fatturazione.thpage("Tipi Prodotto", table="fatt.prodotto_tipo")
     fatturazione.thpage("Prodotti", table="fatt.prodotto")
     fatturazione.thpage("Fatture", table="fatt.fattura")
     fatturazione.lookups("Tabelle Ausiliarie", lookup_manager="fatt")
-
     root.branch("Amministrazione", tags="admin", pkg="adm")
     root.branch("Geo Italia", tags="admin", pkg="glbl")
     root.branch("Sistema", tags="sysadmin", pkg="sys")
     root.branch("Developer", tags="_DEV_", pkg="dev")
-    root.branch("Widgetpedia", dir="widgetpedia",pkg='dev')
