829012e12f87c6c53786ea9c668f782872c71526
Francesco Porcari <francesco.porcari@softwell.it>
2015-06-23 12:14:31 +0200

    no message

--- packages/fatt/model/_packages/ripa/apparato.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
-class Table(object):
-    def config_db(self, pkg):
-        tbl = pkg.table('apparato')
-        tbl.column('cliente', group='_', 
-                    name_long='!!Cliente', 
-                    name_short='Cliente').relation('fatt.cliente.id',relation_name='apparato_clientefk',mode='foreignkey',onDelete='raise')
