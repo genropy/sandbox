67aca5628c97eb9b8e330d490e8533f805ddd946
Francesco Porcari <francesco.porcari@softwell.it>
2017-04-01 12:52:57 +0200

    a

--- packages/tutor/menu.py
+++ packages/tutor/menu.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
 def config(root,application=None):
+    root.branch(u"Lezioni", pkg="tutor", dir="lessions")
     root.branch(u"Esempi", pkg="tutor", dir="examples")
     root.branch(u"Esercizi", pkg="tutor", dir="exercises")
