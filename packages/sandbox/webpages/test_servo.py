c4a0267f3b935449c06cb9ab7549d48ffd5e46af
Michele Bertoldi <michele.bertoldi@softwell.it>
2017-04-09 08:59:44 +0200

    Canvas in meno

--- packages/sandbox/webpages/test_servo.py
+++ packages/sandbox/webpages/test_servo.py
@@ -8,7 +8,7 @@ class GnrCustomWebPage(object):
         form_pane = bc.contentPane(region='left', width='50%')
         root.sharedObject('main.shared', shared_id='servo',autoLoad=True,autoSave=True,expire=20)
 
-        canvas_pane = bc.contentPane(region='center')
+        steps_pane = bc.contentPane(region='center', datapath='main.record')
         fb = form_pane.formbuilder(cols=4 )
         #fb.textBox(lbl='Client ID', value='^page_id', colspan=2)
 
@@ -27,34 +27,18 @@ class GnrCustomWebPage(object):
             fb.horizontalSlider(lbl='Servo %i'%i,value='^.s_%i'%i,width='400px',
                 minimum=1, maximum=100, intermediateChanges=True, colspan=3)
             fb.numberTextBox(value='^.s_%i'%i,places=0, readOnly=True)
-            #fb.dataController("""
-            #    genro.wsk.setInClientData(page_id,'position.s_%i',position);"""%i,
-            #    page_id='=page_id', position='^position.s_%i'%i, _if='position')
-        canvas_pane.canvas(width="400", height="400", id='mycanvas')
-        canvas_pane.dataController("""
-            if (!point) return;
-            var x = point[0];
-            var y = point[1];
-            var r = Math.floor(255*point[2]);
-            var g = Math.floor(255*point[3]);
-            var b = Math.floor(255*point[4]);
-            
-            var d = 7.5;
-            var canvas = document.getElementById('mycanvas');
-            var context = canvas.getContext('2d');
-            if (x==-1 && y==-1){
-                context.clearRect(0, 0, canvas.width, canvas.height);
-            }
-            else{
-            context.beginPath();
-            console.log(x/4-d/2);
-            console.log(y/4-d/2);
-            
-            context.arc(x/4-d/2, canvas.height-y/4-d/2, d, 0, 2 * Math.PI, false);
-            var fillStyle = 'rgb('+r+','+g+','+b+')';
-            console.log(fillStyle);
-            context.fillStyle = fillStyle;
-            context.fill();
-            }
-        """, point='^.point')
-         
+        
+#        frame = steps_pane.bagGrid(storepath='.store',
+#                        title='Passi',
+#                        datapath='.steps',
+#                        struct=self.refstruct,
+#                        addrow=False,delrow=False,
+#                        height='300px')
+#
+#
+#
+#    def steps_struct(self,struct):
+#        r = struct.view().rows()
+#        for i in range(6):
+#            r.cell('s_%i'%i,name='Servo %i'%i)
+#    
