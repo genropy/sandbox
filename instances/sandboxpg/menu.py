c6ef6c6621c7e1f3ef584131591ba13b6cca920e
Francesco Porcari <francesco.porcari@softwell.it>
2017-06-16 20:19:47 +0200

    no message

--- instances/sandboxpg/menu.py
+++ instances/sandboxpg/menu.py
@@ -10,6 +10,7 @@ def config(root,application=None):
     root.branch("Widgetpedia", dir="widgetpedia",pkg='dev')
     root.branch("Fatturazione",pkg='fatt')
     root.branch("Amministrazione", tags="admin", pkg="adm")
+    root.branch("Files", tags="admin", pkg="flib")
     root.branch("Geo Italia", tags="admin", pkg="glbl")
     root.branch("Sistema", tags="sysadmin,_DEV_", pkg="sys")
     test15 = root.branch("Test pages")
