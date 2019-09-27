da9ee67a37ec5398846140b69694bfef1b1b54cb
Saverio Porcari <>
2019-09-27 11:20:08 +0200

    no message

--- packages/fatt/resources/tables/fattura/print/stampa_fatture.py
+++ packages/fatt/resources/tables/fattura/print/stampa_fatture.py
@@ -3,7 +3,7 @@ from gnr.web.batch.btcprint import BaseResourcePrint
 caption = 'Stampa Fattura'
 
 class Main(BaseResourcePrint):
-
     batch_title = 'Stampa Fattura'
     html_res = 'html_res/fattura_stampa'
+    templates = 'carta_intestata'
 
