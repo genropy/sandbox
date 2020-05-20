c9adb8aa2c62f34f5f596ed5e06c2db0eaa1a06f
Saverio Porcari <>
2020-05-20 10:09:31 +0200

    aggiunte formule column per lezione ORM di LearnGenropy

--- packages/fatt/main.py
+++ packages/fatt/main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # encoding: utf-8
+
 from gnr.app.gnrdbo import GnrDboTable, GnrDboPackage
 
 class Package(GnrDboPackage):
+
     def config_attributes(self):
         return dict(comment='Package demo fatturazione',sqlschema='fatt',language='it',
                     name_short='Fatturazione', name_long='Fatturazione', name_full='Fatturazione')
