5cea850e5f57c4ddbefc332e64f44b10c4c33374
Davide Paci <davide.paci@softwell.it>
2021-02-24 15:04:47 +0100

    Aggiunta stampa Statistiche prodotti

--- packages/fatt/resources/tables/prodotto/html_res/stats_prodotti.py
+++ packages/fatt/resources/tables/prodotto/html_res/stats_prodotti.py
@@ -5,7 +5,8 @@ from gnr.web.gnrbaseclasses import TableScriptToHtml
 
 class Main(TableScriptToHtml):
 
-    row_table = 'fatt.fattura'
+    maintable = 'fatt.prodotto'
+    row_table = 'fatt.prodotto'
     page_width = 210
     page_height = 297
     doc_header_height = 30
@@ -18,7 +19,7 @@ class Main(TableScriptToHtml):
         #Questo metodo definisce il layout e il contenuto dell'header della stampa
         head = header.layout(name='doc_header', margin='5mm', border_width=0)
         row = head.row()
-        row.cell("""<center><div style='font-size:18pt;'><strong>Statistiche fatturato per periodo</strong></div>
+        row.cell("""<center><div style='font-size:18pt;'><strong>Statistiche prodotti</strong></div>
                     <div style='font-size:14pt;'>Esercizio {anno}</div></center>::HTML""".format(anno=self.parameter('anno')))
         if self.parameter('cliente_id'):
             row = head.row()
@@ -38,27 +39,41 @@ class Main(TableScriptToHtml):
                             margin:2mm;
                             }
                             """)
+    def gridData(self):
+        condition=['@fattura_id.anno_fattura=:anno']
+        if self.parameter('cliente_id'):
+            condition.append('@fattura_id.cliente_id=:cliente')
+        where=' AND '.join(condition)
+
+        fatturato_grouped_by_prodotto = self.db.table('fatt.fattura_riga').query(columns="""
+                                            SUM($prezzo_totale) AS prezzo_totale,
+                                            SUM($iva) AS iva,
+                                            @prodotto_id.codice AS codice,
+                                            @prodotto_id.descrizione AS descrizione,
+                                            @prodotto_id.@prodotto_tipo_id.hierarchical_descrizione AS prodotto_gerarchia,
+                                            @prodotto_id.@prodotto_tipo_id.descrizione AS prodotto_tipo""",
+                                            where=where,
+                                            anno=self.parameter('anno'),
+                                            cliente=self.parameter('cliente_id'),
+                                            group_by="""@prodotto_id.codice, @prodotto_id.descrizione, 
+                                            @prodotto_id.@prodotto_tipo_id.hierarchical_descrizione,
+                                            @prodotto_id.@prodotto_tipo_id.descrizione""").fetch()
+
+        for prodotto in fatturato_grouped_by_prodotto:
+            categoria_principale = prodotto['prodotto_gerarchia'].split('/')[0]
+            prodotto['categoria_principale'] = categoria_principale
+        
+        return fatturato_grouped_by_prodotto
 
     def gridStruct(self,struct):
         #Questo metodo definisce la struttura della griglia di stampa definendone colonne e layout
         r = struct.view().rows()
-        r.fieldcell('data', mm_width=15)
-        r.fieldcell('mese_fattura', hidden=True, subtotal='Totale {breaker_value}', subtotal_order_by="$data")
-        #Questa formulaColumn verrà utilizzata per creare i subtotali per mese
-        r.fieldcell('protocollo', mm_width=20, name='Documento')
-        r.fieldcell('cliente_id', mm_width=0)
-        r.fieldcell('totale_imponibile', mm_width=20, totalize=True)
-        r.fieldcell('totale_iva', mm_width=20, totalize=True)
-        r.fieldcell('totale_fattura', mm_width=20, totalize=True)
-
-    def gridQueryParameters(self):
-        #Questo metodo fornisce a gridData i parametri (condizioni, relation, table) sulla base dei quali costruire 
-        #le righe con i dati da riportare in griglia. In questo caso uso una condizione.
-        condition=['$anno_fattura=:anno']
-        if self.parameter('cliente_id'):
-            condition.append('$cliente_id=:cliente')
-        return dict(condition=' AND '.join(condition), condition_anno=self.parameter('anno'), 
-                        condition_cliente=self.parameter('cliente_id'))
+        r.fieldcell('codice', mm_width=15)
+        r.fieldcell('descrizione', mm_width=0)
+        r.cell('prodotto_tipo', mm_width=20, name='Tipo prodotto')
+        r.cell('categoria_principale', hidden=True, subtotal='Totale {breaker_value}')
+        r.cell('prezzo_totale', mm_width=20, name='Fatturato tot.', totalize=True)
+        r.cell('iva', mm_width=20, name='IVA tot.',totalize=True)
 
     def docFooter(self, footer, lastPage=None):
         #Questo metodo definisce il layout e il contenuto dell'header della stampa
@@ -73,8 +88,8 @@ class Main(TableScriptToHtml):
         if ext and not ext[0] == '.':
             ext = '.%s' % ext
         if self.parameter('cliente_id'):
-            doc_name = 'Fatturato_{anno}_{cliente}{ext}'.format(anno=self.parameter('anno'), 
+            doc_name = 'Prodotti_{anno}_{cliente}{ext}'.format(anno=self.parameter('anno'), 
                         cliente=self.parameter('ragione_sociale'), ext=ext)
         else: 
-            doc_name = 'Fatturato_{anno}{ext}'.format(anno=self.parameter('anno'), ext=ext)
+            doc_name = 'Prodotti_{anno}{ext}'.format(anno=self.parameter('anno'), ext=ext)
         return doc_name
