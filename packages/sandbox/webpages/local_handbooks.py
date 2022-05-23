75dc4368806d434e65a1005dafe22a1fa8d1f5d5
Davide Paci <davide.paci@softwell.it>
2022-05-23 12:50:05 +0200

    Fix splashscreen for local handbooks

--- packages/sandbox/webpages/local_handbooks.py
+++ packages/sandbox/webpages/local_handbooks.py
@@ -24,13 +24,14 @@ class GnrCustomWebPage(object):
             r.td(row['title'],text_align='right')
             localfolder = local_handbooks.child(row['name'])
             r.data('.downloaded',localfolder.exists)
+            download_url = row['download_url']
             r.td().lightbutton('^.downloaded?=#v?"Update":"Download"' ,
-                    action="FIRE .dl = download_url;",padding='5px',background='rgba(234, 203, 110, 1)',rounded=6,
-                    download_url=row['download_url'],width='10em',text_align='center')
+                    padding='5px',background='rgba(234, 203, 110, 1)',rounded=6,
+                    download_url=download_url,width='10em',text_align='center'
+                    ).dataRpc('.downloaded',self.downloadInteractiveHandbook, download_url=download_url, _lockScreen=True)
             r.td().lightbutton('Apri manuale',action="genro.openWindow(indexurl)",hidden='^.downloaded?=!#v',
                     width='10em',text_align='center',padding='5px',background='rgba(234, 203, 110, 1)',rounded=6,
                     indexurl=localfolder.child('index.html').url())
-            r.dataRpc('.downloaded',self.downloadInteractiveHandbook, handbook_url='^.dl', _lockScreen=True)
 
         root.div("""Una volta effettuato l'update, assicurati di aver svuotato la cache per visualizzare la versione aggiornata della documentazione""",
                     padding='40px 40px 10px 40px', font_size='14px')
@@ -40,15 +41,15 @@ class GnrCustomWebPage(object):
     
     @public_method
     def interactiveHandbooks(self,project_code=None,package_identifier=None,
-                            table_identifier=None,pagename=None,**kwargs):
+                                    table_identifier=None,pagename=None,**kwargs):
         result = NetBag(self.docsUrl(),'get_interactive_handbooks')()
         return result
 
     @public_method
-    def downloadInteractiveHandbook(self,handbook_url=None,**kwargs):
-        filezip = handbook_url.split('/')[-1]
-        r = requests.get(handbook_url, stream=True)
-        if r.status_code == 200:
+    def downloadInteractiveHandbook(self, download_url=None,**kwargs):
+        filezip = download_url.split('/')[-1]
+        r = requests.get(download_url, stream=True)
+        if r.ok:
             with self.site.storageNode('site:local_handbooks',filezip).open('wb') as f:
                 for chunk in r.iter_content(1024):
                     f.write(chunk)
