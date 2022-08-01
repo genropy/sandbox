e88017f9cca4563313f551357d74be68871d4893
fporcari <francesco.porcari@softwell.it>
2022-08-01 19:10:25 +0200

    f

--- packages/tutor/webpages/lessons/6_le_stampe/2_layout.py
+++ packages/tutor/webpages/lessons/6_le_stampe/2_layout.py
@@ -8,9 +8,9 @@ class GnrCustomWebPage(object):
         l = page.layout(top=1,left=1,
                     bottom=1,right=1,border_width=0.3)
         l.row().cell()
-        s = l.row().cell().layout(top=2,left=2,
-                                  right=2,
-                                  bottom=2,border_width=0.3)
+        s = l.row().cell().layout(top=20,left=20,
+                                  right=20,
+                                  bottom=10,border_width=0.3)
         l.row().cell()
         inner_row = s.row()
         inner_row.cell()
