f18184607becfa8cad2c6a7a682f6b654ed0b3da
Davide Paci <davide.paci@softwell.it>
2023-11-06 14:29:43 +0100

    Removed pwa and oidc from master, online website will use genropynet branch instead

--- packages/sandbox/resources/preference.py
+++ packages/sandbox/resources/preference.py
@@ -2,5 +2,4 @@
 class AppPref(object):
     def prefpane_sandbox(self,parent,**kwargs):
         tc = parent.tabContainer(margin='2px',**kwargs)
-        self.mixinComponent('pwa:PWAPreferencePane')
-        self.pwaPreferencePane(tc)
+        pass
