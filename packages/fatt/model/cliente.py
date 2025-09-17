fa52ef550044a9b55788d7a5d5946449aceeda2e
Christopher R. Gabriel <christopher.gabriel@gmail.com>
2025-09-17 17:38:25 +0200

    added subtables to fatt.cliente table, based on startup data, to query "Privati" or "Negozi" using subtable feature

--- packages/fatt/model/cliente.py
+++ packages/fatt/model/cliente.py
@@ -7,6 +7,8 @@ class Table(object):
         tbl = pkg.table('cliente', pkey='id', name_long='!![it]Cliente', 
                         name_plural='!![it]Clienti',caption_field='ragione_sociale',
                         branch_field='categoria')
+        tbl.subtable("privati", condition="$cliente_tipo_codice='PV'")
+        tbl.subtable("negozi", condition="$cliente_tipo_codice='NG'")
         self.sysFields(tbl) # aggiunge id autogenerato, __ins_ts,__mod_ts,etc.
         tbl.column('ragione_sociale' ,size=':40',name_long='!![it]Ragione sociale',name_short='Rag. Soc.',validate_notnull=True,validate_len='2:40')
         tbl.column('indirizzo',name_long='!![it]Indirizzo')
@@ -16,7 +18,9 @@ class Table(object):
                             relation_name='clienti',
                             mode='foreignkey',onDelete='raise')
         tbl.column('comune_id',size='22' ,group='_',name_long='!![it]Comune').relation('glbl.comune.id',relation_name='clienti',mode='foreignkey',onDelete='raise')
+        
         tbl.column('cliente_tipo_codice',size=':5',name_long='!![it]Tipo cliente',name_short='!![it]Tipo').relation('cliente_tipo.codice',relation_name='clienti',mode='foreignkey',onDelete='raise')
+        
         tbl.column('pagamento_tipo_codice',size=':5',name_long='!![it]Tipo pagamento',name_short='!![it]Tipo pagamento').relation('pagamento_tipo.codice',relation_name='clienti',mode='foreignkey',onDelete='raise')
         tbl.column('note',name_long="!![it]Note")
         tbl.column('email',name_long='!![it]Email')
