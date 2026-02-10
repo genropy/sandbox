7f0ce620fc6fb54107cfd19348d86f5092a17f36
fporcari <francesco.porcari@softwell.it>
2026-02-10 10:20:01 +0100

    fix

--- packages/shop/resources/tables/shop/th_shop.py
+++ packages/shop/resources/tables/shop/th_shop.py
@@ -61,7 +61,7 @@ class Form(BaseComponent):
         fb.field('denominazione')
         fb.field('partita_iva')
         fb.div('^.dbstore',hidden='^.dbstore?=!#v')
-        fb.button('Crea',hidden='^.dbstore',disabled='^#FORM.controller.status?=#v!="ok"').dataRpc(self.creaDbShop,
+        fb.button('Crea',hidden='^.active_dbstore',disabled='^#FORM.controller.status?=#v!="ok"').dataRpc(self.creaDbShop,
                                                      pkey='=#FORM.record.id',
                                                      _lockScreen=True,
                                                     _ask=dict(title='Crea dbstore',
@@ -71,53 +71,29 @@ class Form(BaseComponent):
                                                                 validate_notnull=True,
                                                                 validate_regex_error='Caratteri non ammessi')]),
                                                             _onResult="this.form.reload()")
-       #fb.button('import dbtemplate',hidden='^.imported_instance',
-       #                 ask=dict(title=u'Importa dati da', hidden='^#FORM.record.dbtemplate',
-       #                                        fields=[dict(name='dbtemplate', lbl='A partire da',
-       #                                                     tag='filteringselect',
-       #                                                     storepath='dbtemplates', storeid='filepath',
-       #                                                     storecaption='caption')]),
-       #          action='FIRE #FORM.import_data=dbtemplate')
+        fb.div(hidden='^.startup_data_ts').button('Inizializza dati',
+                                                    hidden='^.active_dbstore?=!#v').dataRpc(
+                                                        self.startupDataInWorkspace,
+                                                        timeout=0,
+                                                        pkey='=#FORM.record.id',
+                                                        _lockScreen=dict(thermo=True),
+                                                        _onResult="this.form.reload()"
+                                                    )
 
-       #fb.dataRpc(None, self.importaDatiDaTemplate, dbtemplate='^#FORM.import_data', pkey='=#FORM.pkey',
-       #           _onResult='this.form.reload()', _lockScreen=True)
+
         tc = bc.tabContainer(region='center', margin='2px',selectedPage='^#FORM.selectedPage')
         tc.contentPane(title='Dati anagrafici')
         tc.checkSyncDataTab(title='Controllo dati',hidden='^#FORM.record.dbstore?=!#v')
     
-    def gestioneUtenzeEsterne(self,pane):
-        th = pane.dialogTableHandler(table='adm.user',condition='$dbstore=:dbs',
-                                dialog_height='400px',formInIframe='/adm/user_page',
-                                dialog_width='650px',condition_dbs='^#FORM.record.dbstore')
-        bar = th.view.top.bar.replaceSlots('addrow','creautente')
 
-        bar.creautente.slotButton(u'Crea utente shop',
-                        iconClass='iconbox add_row',
-                        email='=#FORM.record.@cliente_id.@anagrafica_id.email',
-                        action='PUBLISH creaUtenteContabilita = {"user":user,"password":password,"email":email};',
-                        ask=dict(title="Dati utenza",
-                                fields=[dict(name='user',
-                                             lbl='User',validate_notnull=True),
-                                        dict(name='password',type='password',
-                                            lbl='Password',validate_notnull=True),
-                                        dict(name='email',lbl='Email')]),
-                                hidden='^.user_id')
-       #pane.dataRpc(None,self.db.table('erpy_studio.contabilita').creaUtenteContabilita,subscribe_creaUtenteContabilita=True,
-       #            record_id='=#FORM.record.id',_if='record_id')
-    
-    
+        #                ).thIframe(self.iframe_datiSocieta,dbstore='^#FORM.record.dbstore')
 
-   #@public_method
-   #def importaDatiDaTemplate(self, pkey=None, dbtemplate=None):
-   #    self.mixinComponent('startupdata_manager/startupdata_manager:StartupDataManager')
-   #    dbstore = self.db.table('erpy_studio.contabilita').readColumns(
-   #        pkey=pkey, columns='$dbstore')
-   #    with self.db.tempEnv(storename=dbstore):
-   #        self.sd_loadDbTemplate(dbtemplate)
-   #        self.db.table('adm.counter').initializeTableSequences(self.db.table('erpy_coge.conto'))
-   #    with self.db.table('erpy_studio.contabilita').recordToUpdate(pkey) as rec:
-   #        rec['dbtemplate'] = os.path.basename(dbtemplate).split('.')[0]
-   #        self.db.commit()
+
+    @public_method
+    def startupDataInWorkspace(self,pkey=None):
+        tblobj = self.db.table('shop.shop')
+        tblobj.multidb_setStartupData(pkey)
+
 
     @public_method
     def creaDbShop(self,pkey=None,dbstore=None):
