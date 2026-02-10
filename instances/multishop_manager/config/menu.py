14af86623d7286a5eeb0646de487b0e39dd596b8
Christopher R. Gabriel <christopher.gabriel@gmail.com>
2026-02-10 10:09:03 +0100

    removed unused instance as confirmed by @fporcari

--- instances/multishop_manager/config/menu.py
+++
@@ -1,6 +0,0 @@
-# encoding: utf-8
-class Menu(object):
-    def config(self,root,**kwargs):
-        root.packageBranch(u"Amministrazione", tags="admin", pkg="adm")
-        root.packageBranch(u"Sistema", tags="sysadmin,_DEV_", pkg="sys")
-        root.thpage('Shop',table='shop.shop',aux_instance='multishop')
